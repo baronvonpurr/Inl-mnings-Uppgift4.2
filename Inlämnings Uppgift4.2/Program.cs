12875645647a4cb8357a7a07faa03a50ef9a8c44
baronvonpurr <robinhood.svensson@hotmail.com>
2023-10-18 10:52:14 +0200

    Add project files.

---
+++ Inlämnings Uppgift4.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace Uppgift4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Tal;
+            string Mer;
+            int Stor = 0;
+            do
+            {
+                Console.WriteLine("Skriv ett heltal");
+                Tal = int.Parse(Console.ReadLine());
+                Console.WriteLine("Vill du skriva ett till heltal? (y)");
+                Mer = Console.ReadLine().ToLower();
+                if (Tal > Stor)
+                {
+                    Stor = Tal;
+                }
+            } while (Mer == "y");
+            Console.WriteLine(Stor);
+            Console.ReadKey();
+        }
+    }
+}
